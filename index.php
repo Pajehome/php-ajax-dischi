566f30dfe5647a80c171f576265b45ca160491dd
Pajehome <abdel20069@gmail.com>
2022-05-25 14:45:03 +0200

    init

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+include 'server.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+ foreach($dischi as $value){?>
+
+<h2><?php echo $value['title']; ?></h2>
+
+ <?php } ?>
+
+
+</body>
+</html>
